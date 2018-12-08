e3bcadbfd67d6c5784eb5076ce7f043de91f1887
jake anderson <you@example.com>
2018-12-08 13:48:17 -0800

    add meta tags to homepage for seo for facebook twitter and general

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
 ];
 
 NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [ RouterModule.forRoot(routes, { useHash: false }) ],
   exports: [ RouterModule ]
 });
 
